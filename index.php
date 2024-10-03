40e5d315fc0da763c18a779cf32fe6a37b690d65
Giackpetro <giacomopetrini2006@gmail.com>
2024-10-03 17:13:11 +0200

    es finito

--- index.php
+++ index.php
@@ -6,7 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+<?php
+    $lar = rand(50, 500);
+
+    function sfondoCasuale($larghezza) {
+        if ($larghezza < 150) {
+            return "#7FFFD4";
+        } elseif ($larghezza <= 250) {
+            return "#1E90FF";
+        } else {
+            return "#0000FF";
+        }
+    }
+
+    $altezza = $lar * 2 . "px";
+    $colore = sfondoCasuale($lar);
+    $larghezza = $lar . "px";
+    
+    echo "<div style='height: $altezza ; width: $larghezza ; background-color: $colore;'></div>";
     ?>
 </body>
 </html>
