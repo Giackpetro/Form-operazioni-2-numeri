39d7ca51a0cf4fe46621ccfd9abe3ca01999c56f
Giackpetro <giacomopetrini2006@gmail.com>
2024-10-06 19:33:17 +0200

    es finito

--- index.php
+++ index.php
@@ -6,7 +6,36 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    ?>
+    <style>
+    table{
+        border: 1px solid black;
+          border-collapse: collapse;
+     }
+    td{
+        padding: 10px;border: 1px solid black;
+    }
+</style>
+<?php
+$righe = 30;
+$colonne = 12;
+    
+stampaTabella($righe, $colonne);
+
+function stampaTabella($m, $n){
+    $vr = array();
+    $arr = array();
+    echo("<table  >");
+        for ($i=0; $i < $m ; $i++) {
+            echo("<tr>"); 
+            $arr[$i] = $i;
+            for ($j=0; $j < $n ; $j++) { 
+                $vr[$j]="C".$j;
+                echo("<td>".$arr[$i]."-".$vr[$j]."</td>");
+            }
+            echo("</tr>");
+        }
+    echo("</table>");
+    }
+?>
 </body>
 </html>
